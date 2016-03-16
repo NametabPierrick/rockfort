37405b1fa44c8a35462c9220d0afd9522daf4193
JimCaignard <jim.caignard@gmail.com>
2016-03-16 17:23:22 +0100

    Update All project

--- js/init.js
+++ js/init.js
@@ -1,5 +1,13 @@
 $(document).ready( function () {
 	
+
+	$(".subMenu").hide();
+
+	$(".fleche_bottom").click(function(){
+		$(this).parent().find(".subMenu").slideToggle();
+	});
+
+
 	//slider
 	$('.single-slider').jRange({
 		from: -2.0,
@@ -284,5 +292,97 @@ $.post(window.location.pathname, {"id_morceau":idsong,"periode":periode}, functi
 	
 // }
 
-    
 }
+
+
+// IMPORT STAFF EVE
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+
+// wavesurfer.load('Rolling Stones-GimmeShelter.mp3');
+
+
+// Equalizer
+wavesurfer.on('ready', function () {
+    var EQ = [
+        {
+            f: 32,
+            type: 'lowshelf'
+    }, {
+            f: 64,
+            type: 'peaking'
+    }, {
+            f: 125,
+            type: 'peaking'
+    }, {
+            f: 250,
+            type: 'peaking'
+    }, {
+            f: 500,
+            type: 'peaking'
+    }, {
+            f: 1000,
+            type: 'peaking'
+    }, {
+            f: 2000,
+            type: 'peaking'
+    }, {
+            f: 4000,
+            type: 'peaking'
+    }, {
+            f: 8000,
+            type: 'peaking'
+    }, {
+            f: 16000,
+            type: 'highshelf'
+    }
+  ];
+
+    // Create filters
+    var filters = EQ.map(function (band) {
+        var filter = wavesurfer.backend.ac.createBiquadFilter();
+        filter.type = band.type;
+        filter.gain.value = 0;
+        filter.Q.value = 1;
+        filter.frequency.value = band.f;
+        return filter;
+    });
+
+    // Connect filters to wavesurfer
+    wavesurfer.backend.setFilters(filters);
+
+    // Bind filters to vertical range sliders
+    var container = document.querySelector('#equalizer');
+    filters.forEach(function (filter) {
+        var input = document.createElement('input');
+        wavesurfer.util.extend(input, {
+            type: 'range',
+            min: -40,
+            max: 40,
+            value: 0,
+            title: filter.frequency.value
+        });
+        input.style.display = 'inline-block';
+        input.setAttribute('orient', 'vertical');
+        wavesurfer.drawer.style(input, {
+            'webkitAppearance': 'slider-vertical',
+            width: '50px',
+            height: '150px'
+        });
+        container.appendChild(input);
+
+        var onChange = function (e) {
+            filter.gain.value = ~~e.target.value;
+        };
+
+        input.addEventListener('input', onChange);
+        input.addEventListener('change', onChange);
+    });
+
+    // For debugging
+    wavesurfer.filters = filters;
+});
